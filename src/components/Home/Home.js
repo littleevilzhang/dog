ddff41cce1fef4e88ebba6cd03e2b0829903d84f
Your Name <you@example.com>
2018-02-26 11:11:46 +0800

    is a dog

---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React , { Component } from 'react'
+import './home.css'
+import DogIcon from '../DogIcon/DogIcon'
+
+
+class Home extends Component {
+  state = {
+    dogs : [ ]
+  }
+  render () {
+    const dogList = this.state.dogs.map(t =>(
+      <div key={t.id} className="dog-icon-wrap">
+        <DogIcon dog={t} />
+      </div>
+    ))
+    return (
+      <div className='home'>
+        <div className="dog-list">
+          {dogList}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
